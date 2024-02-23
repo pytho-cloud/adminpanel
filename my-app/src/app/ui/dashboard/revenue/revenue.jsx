37be32663ee2227dc1d3743afaf57e1a357e02e2
inverse <rajnepale25@gmail.com>
2024-02-23 21:56:03 +0530

    Added Spinner

--- my-app/src/app/ui/dashboard/revenue/revenue.jsx
+++ my-app/src/app/ui/dashboard/revenue/revenue.jsx
@@ -5,21 +5,33 @@ import StackAreaChart from './stackarea';
 
 function Revenue() {
   return (
-    <div className="container mx-auto ">
-      <div className="containers flex flex-col md:flex-row  gap-5 p-4 ">
-        <div className="pie-chart w-full   md:w-1/2 flex bg-[#182237] p-3 rounded-3xl">
-          <StackAreaChart/>
+    <div className="container  ">
+      <div className="containers flex flex-col md:flex-row  gap-5 p-4">
+        <div className="pie-chart w-full md:w-1/2 flex bg-[#182237] p-3 rounded-3xl">
+          <AreaCharts />
+          {/* <StackAreaChart/> */}
           
         </div>
+        
         <div className="containers bar-chart w-full md:w-1/2 bg-[#182237] p-3 rounded-3xl">
-          <h1>Salesforecast</h1>
+         
           <BarChart />
         </div>
       </div>
-      <div className="container text-center gap-5 m-3 p-2">
-        <h1 className="w-full text-center p-4 font-bold text-2xl">Revenue</h1>
-        <div className="pie-chart bg-[#182237] p-3 rounded-3xl">
+      <div className="container  gap-2 m-3 ">
+        <h1 className="w-full   font-bold m-3 ">
+         <h2 className='font-bold text-2xl'>
+         Revenue
+        </h2>
+          <p className='font-thin'>
+          Payment recieve across all channels
+          </p>
+           
+          
+          </h1>
+        <div className="pie-chart bg-[#182237] p-4 container rounded-3xl flex items-center pl-3 w-10/12 m-auto   ">
           <AreaCharts />
+          {/* <StackAreaChart/> */}
         </div>
       </div>
     </div>
